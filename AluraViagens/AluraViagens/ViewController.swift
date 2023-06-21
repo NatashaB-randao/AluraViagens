62db9f1d65ba08c28d63fe06734867f64409f0c5
NatashaB-randao <natashabrandao57@hotmail.com>
2023-06-21 14:25:16 -0300

    Table View

---
+++ AluraViagens/AluraViagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AluraViagens
+//
+//  Created by Natasha Brandão on 21/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
