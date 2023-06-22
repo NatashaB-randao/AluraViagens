9664124719b5f2e2080fe77c7eebdb2ca347a3b3
NatashaB-randao <natashabrandao57@hotmail.com>
2023-06-22 13:40:27 -0300

    UITableViewDataSource

--- AluraViagens/AluraViagens/ViewController.swift
+++ AluraViagens/AluraViagens/ViewController.swift
@@ -7,14 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "viagem \(indexPath.row)"
+        
+        return cell 
+    }
 }
+
+
 
 // Tabelas:
 
