87ce1c75fb6b14b01e937221369e24328ccad16a
NatashaB-randao <natashabrandao57@hotmail.com>
2023-07-03 13:56:40 -0300

    Arredondando view

--- AluraViagens/AluraViagens/View/Header/HomeTableViewHeader.swift
+++ AluraViagens/AluraViagens/View/Header/HomeTableViewHeader.swift
@@ -18,9 +18,17 @@ class HomeTableViewHeader: UIView {
     
     @IBOutlet weak var bannerimageView: UIImageView!
     
+    @IBOutlet weak var bannerView: UIView!
+    
     
     func configuraView() {
         headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+        
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 100
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
     
 }
