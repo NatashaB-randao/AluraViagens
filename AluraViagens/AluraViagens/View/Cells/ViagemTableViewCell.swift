4c87a04b8a4e12c9dae82ed2f28f311406750808
NatashaB-randao <natashabrandao57@hotmail.com>
2023-07-03 15:26:29 -0300

    Criando uma nova célula

---
+++ AluraViagens/AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Natasha Brandão on 03/07/23.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
