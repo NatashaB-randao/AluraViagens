c97a171d668359f8402820a97ab7dd1f1b1b7069
NatashaB-randao <natashabrandao57@hotmail.com>
2023-07-04 14:14:12 -0300

    Utilizando mock para listar as viagens

---
+++ AluraViagens/AluraViagens/ViewModel/ViagemDestaqueViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ViagemDestaqueViewModel.swift
+//  AluraViagens
+//
+//  Created by Natasha Brandão on 04/07/23.
+//
+
+import Foundation
+
+class ViagemDestaqueViewModel: ViagemViewModel {
+    
+    var tituloSessao: String {
+        return "Destaques"
+    }
+    
+    var tipo: ViagemViewModelType {
+        return .destaques
+    }
+    
+    var viagens: [Viagem]
+    
+    var numeroDeLinhas: Int {
+        return viagens.count
+    }
+    
+    init(_ viagens: [Viagem]) {
+        self.viagens = viagens
+    }
+    
+}
---
+++ AluraViagens/AluraViagens/ViewModel/ViagemViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ViagemViewModel.swift
+//  AluraViagens
+//
+//  Created by Natasha Brandão on 04/07/23.
+//
+
+import Foundation
+
+enum ViagemViewModelType: String {
+    case destaques
+    case ofertas
+    case internacionais
+}
+
+protocol ViagemViewModel {
+    var tituloSessao: String { get }
+    var tipo: ViagemViewModelType { get }
+    var viagens: [Viagem] { get set }
+    var numeroDeLinhas: Int { get }
+}
